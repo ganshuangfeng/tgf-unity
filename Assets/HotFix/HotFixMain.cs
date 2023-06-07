a477f1f31de231026ba5bb388520e59e06035493
tim.huang <thkhxm@gmail.com>
2023-06-07 19:18:50 +0800

    init project!

---
+++ Assets/HotFix/HotFixMain.cs
@@ -0,0 +1,20 @@
+﻿// /*******************************************************************
+// **File Name:HotFixMain.cs
+// **Author:   tim.huang(thkhxm@gmail.com)
+// **Create Time: 2023-06-07
+// **Desc:
+// *******************************************************************/
+
+using System;
+using UnityEngine;
+
+namespace HotFix
+{
+    public class HotFixMain : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("new script --> 3");
+        }
+    }
+}
---
+++ Assets/HotFix/Main.cs
@@ -0,0 +1,20 @@
+﻿// /*******************************************************************
+// **File Name:Main.cs
+// **Author:   tim.huang(thkhxm@gmail.com)
+// **Create Time: 2023-06-07
+// **Desc:
+// *******************************************************************/
+
+using System;
+using UnityEngine;
+
+namespace HotFix
+{
+    public class Main : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("hotfix 2");
+        }
+    }
+}
